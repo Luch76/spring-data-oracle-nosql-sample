882b17537d25aac5002353a68a11ead5ad5b5c73
Luciano Belotto <DarthLuch@gmail.com>
2024-09-13 11:57:10 -0400

    2024-09-13 - no-sql compartment

--- src/main/java/com/example/NoSQLConfig.java
+++ src/main/java/com/example/NoSQLConfig.java
@@ -1,20 +1,39 @@
 package com.example;
 
+import java.io.File;
 import java.io.IOException;
 
 import com.oracle.nosql.spring.data.config.AbstractNosqlConfiguration;
 import com.oracle.nosql.spring.data.config.NosqlDbConfig;
 import com.oracle.nosql.spring.data.repository.config.EnableNosqlRepositories;
+import oracle.nosql.driver.NoSQLHandleConfig;
 import oracle.nosql.driver.iam.SignatureProvider;
+import com.oracle.nosql.spring.data.core.NosqlTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 @EnableNosqlRepositories
 public class NoSQLConfig extends AbstractNosqlConfiguration {
     @Value("${cloud.oci.region}")
     String region;
+
+    @Value("${cloud.oci.compartment-id}")
+    String compartmentId;
+
+    @Value("${cloud.oci.tenancy-id}")
+    String tenancyId;
+
+    @Value("${cloud.oci.user-id}")
+    String userId;
+
+    @Value("${cloud.oci.private-key-file}")
+    String privateKeyFileString;
+
+    @Value("${cloud.oci.fingerprint-of-key-file}")
+    String fingerprintOfKeyFile;
 
     /**
      * Creates a NosqlDbConfig using the default OCI config file at ~/.oci/config
@@ -24,7 +43,24 @@ public class NoSQLConfig extends AbstractNosqlConfiguration {
      */
     @Bean
     public NosqlDbConfig nosqlDbConfig() throws IOException {
-        //return new NosqlDbConfig(region, new SignatureProvider());
-        return NosqlDbConfig.createCloudConfig(region, "~/.oci/config", "NOSQL");
+        SignatureProvider provider;
+        NosqlDbConfig nosqlDbConfig;
+        File privateKeyFile;
+        NoSQLHandleConfig nosqlHandleConfig;
+        char[] passPhrase = new char[0];
+
+        //nosqlDbConfig = NosqlDbConfig.createCloudConfig(region, "~/.oci/config", "NOSQL");
+
+        privateKeyFile = new File(privateKeyFileString);
+        provider = new SignatureProvider(tenancyId, userId, fingerprintOfKeyFile, privateKeyFile, passPhrase);
+        nosqlHandleConfig = new NoSQLHandleConfig(region, provider);
+        nosqlHandleConfig.setDefaultCompartment(compartmentId);
+        nosqlDbConfig = new NosqlDbConfig(nosqlHandleConfig);
+
+
+        return nosqlDbConfig;
     }
+
 }
+
+
